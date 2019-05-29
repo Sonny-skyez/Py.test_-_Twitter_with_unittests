01067525bf4f59002fa7e94451f1d723b6244455
SonnySkyez <hnic85@gmail.com>
2019-05-29 16:48:12 +0200

    Add Twitter script and it's unit tests in py.test (contains test parametrization, fixtures, fixtures parametrization)

---
+++ twitter_test.py
@@ -0,0 +1,35 @@
+from twitter import Twitter
+import pytest
+
+
+@pytest.fixture(params = [None, 'test.txt'], name='twitter')
+def fixture_twitter(request):
+    twitter = Twitter(backend = request.param)
+    yield twitter
+    twitter.delete()
+
+
+def test_twitter_initialization(twitter):
+    assert twitter
+
+
+def test_tweet_single_message(twitter):
+    twitter.tweet('Test message')
+    assert twitter.tweets == ['Test message']
+
+
+def test_tweet_long_message(twitter):
+    with pytest.raises(Exception):
+        twitter.tweet('test'*41)
+    assert twitter.tweets == []
+
+
+@pytest.mark.parametrize('message, expected', (
+        ('Test #first message', ['first']),
+        ('#first test message', ['first']),
+        ('#FIRST test message', ['first']),
+        ('test message #first', ['first']),
+        ('test message #first #second', ['first', 'second'])
+))
+def test_tweet_with_hashtag(twitter, message, expected):
+    assert twitter.find_hashtags(message) == expected
